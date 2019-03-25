b42f0f6bb89580afc19197599ad94e6887c71904
kroko <code@kroko.me>
2019-03-25 04:02:31 +0200

    stuff

--- HelloFBdev/projectfiles/fbdev-gcc-make-allwinner_h3/run-helper.sh
+++ HelloFBdev/projectfiles/fbdev-gcc-make-allwinner_h3/run-helper.sh
@@ -8,5 +8,6 @@ realpath_normalized_abs() {
 PATH_SCRIPT_FILE=$(realpath_normalized_abs $0)
 PATH_SCRIPT_DIR=$(dirname $PATH_SCRIPT_FILE)
 
-GL_KEYBOARD=/dev/input/event1 GL_MOUSE=/dev/input/event0 bash ${PATH_SCRIPT_DIR}/run.sh --width 1920 --height 1080
+bash ${PATH_SCRIPT_DIR}/run.sh --width 1920 --height 1080
+# GL_KEYBOARD=/dev/input/event1 GL_MOUSE=/dev/input/event0 bash ${PATH_SCRIPT_DIR}/run.sh --width 1920 --height 1080
 # GL_KEYBOARD=/dev/input/event1 GL_MOUSE=/dev/input/event0 bash ${PATH_SCRIPT_DIR}/run.sh --width 1280 --height 720
